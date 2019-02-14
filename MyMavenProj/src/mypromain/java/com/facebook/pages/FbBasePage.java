1257a682744a1f4a089e35ebbfe84b6a2f42a06f
subhashautomation27 <subhashautomation27@gmail.com>
2019-02-14 10:34:29 +0530

    this is my second maven code commit

---
+++ MyMavenProj/src/mypromain/java/com/facebook/pages/FbBasePage.java
@@ -0,0 +1,15 @@
+package com.facebook.pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class FbBasePage {
+	 WebDriver driver;
+	
+	public void initialize()
+	{
+		System.setProperty("webdriver.chrome.driver", "E:\\workspace for eclipse\\MyMavenProj\\Drivers\\chromedriver.exe");
+	}
+	
+	
+
+}
